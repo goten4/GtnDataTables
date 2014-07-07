4ce8a84be1ec76f4668c158945ed039a0e2a7a9c
goten4 <goten4@gmail.com>
2014-07-07 17:00:59 +0200

    Add getConfig() in Module.php

--- src/GtnDataTables/Module.php
+++ src/GtnDataTables/Module.php
@@ -4,9 +4,18 @@ namespace GtnDataTables;
 use Zend\Loader\AutoloaderFactory;
 use Zend\Loader\StandardAutoloader;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
-class Module implements AutoloaderProviderInterface
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
+    /**
+     * @return array|mixed|\Traversable
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
     /**
      * @return array
      */
